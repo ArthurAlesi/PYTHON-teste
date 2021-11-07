e76dd6bd1cc5d9e5200fa496dfa8018d982ecf12
ArthurAlesi <54421573+ArthurAlesi@users.noreply.github.com>
2021-11-07 18:31:00 -0300

    Create teste5.py

---
+++ teste5.py
@@ -0,0 +1,20 @@
+print("ola mundo 5")
+
+for x in range(30):
+    print(x)
+
+
+y = 2
+if y == 2:
+    print("alo mundo")
+else:
+    print("hello world")
+
+
+class C():
+
+    def __init__(self):
+        self.x = 10
+
+c = C()
+print("x é ", c.x)
